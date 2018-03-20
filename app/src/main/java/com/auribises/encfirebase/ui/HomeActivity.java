c32a792ec83f3c31ef2533c26dc8ba60c1ac10f3
Ishant Kumar <er.ishant@gmail.com>
2018-03-20 12:27:06 +0530

    FirebaseFirestore Signout

--- app/src/main/java/com/auribises/encfirebase/ui/HomeActivity.java
+++ app/src/main/java/com/auribises/encfirebase/ui/HomeActivity.java
@@ -1,8 +1,13 @@
 package com.auribises.encfirebase.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import com.auribises.encfirebase.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +15,32 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_home,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if(id == R.id.signOut){
+
+            FirebaseAuth auth = FirebaseAuth.getInstance();
+            auth.signOut();
+
+            Intent intent = new Intent(HomeActivity.this,RegisterUserActivity.class);
+            startActivity(intent);
+            finish();
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
